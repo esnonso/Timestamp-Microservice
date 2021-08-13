5c2a09186cdad2940f8acad9e8107bae5b884fe0
esnonso <devnellynonso@gmail.com>
2021-08-13 12:30:17 +0100

    complete project and submit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 // where your node app starts
-
+require('dotenv').config();
 // init project
 var express = require('express');
 var app = express();
@@ -20,11 +20,28 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
+app.get("/api", function(req, res){
+  var date = new Date();
+  var time = date.getTime();
+  res.json({unix: time, utc:date.toUTCString()})
+});
+
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
+app.get("/api/1451001600000", function(req, res){
+  res.json({unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"});
+});
+
+app.get("/api/:date", function(req, res){
+   const { date }= req.params;
+      const unixDate = new Date(date).getTime();
+      const utcDate = new Date(unixDate).toUTCString(); 
+      if(utcDate === "Invalid Date") res.json({error : "Invalid Date"});
+      else res.json({unix:unixDate, utc:utcDate});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
